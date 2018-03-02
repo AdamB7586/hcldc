f7c234a52dabcddbc93668055347e176769fcb88
Adam Binnersley <abinnersley@gmail.com>
2018-03-02 14:03:01 +0000

    Add unsigned option to mysql database

--- database/mysql_database.sql
+++ database/mysql_database.sql
@@ -1,22 +1,22 @@
 CREATE TABLE IF NOT EXISTS `highway_code_section` (
-  `sec_no` int(11) NOT NULL AUTO_INCREMENT,
+  `sec_no` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `title` varchar(1000) NOT NULL,
   PRIMARY KEY (`sec_no`)
 );
 
 CREATE TABLE IF NOT EXISTS `highway_code` (
-  `hcno` int(11) NOT NULL AUTO_INCREMENT,
+  `hcno` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `hcrule` longtext,
   `hctitle` varchar(255) DEFAULT NULL,
   `imagetitle1` varchar(31) DEFAULT NULL,
   `imagetitle2` varchar(31) DEFAULT NULL,
   `imagetitle3` varchar(31) DEFAULT NULL,
   `imagefooter1` text,
-  `pub` varchar(1) DEFAULT NULL,
-  `pubsec` smallint(6) DEFAULT NULL,
-  `pubsubsec` smallint(6) DEFAULT NULL,
+  `pub` varchar(1) UNSIGNED DEFAULT NULL,
+  `pubsec` smallint(6) UNSIGNED DEFAULT NULL,
+  `pubsubsec` smallint(6) UNSIGNED DEFAULT NULL,
   `largetext` longtext,
-  `module` smallint(6) DEFAULT NULL,
+  `module` smallint(6) UNSIGNED DEFAULT NULL,
   PRIMARY KEY (`hcno`),
   UNIQUE KEY `hcno` (`hcno`)
 );
